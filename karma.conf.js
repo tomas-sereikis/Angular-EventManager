a69e6b582dda96cf06c49ef6029d4da9cd004c9b
Tomas-Sereikis <tomas.sereikis@gmail.com>
2014-10-08 20:20:33 +0300

    small changes for coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            'src/*.js': ['coverage']
+            'src/eventManager.js': 'coverage'
         },
 
 
@@ -41,8 +41,8 @@ module.exports = function (config) {
 
         // optionally, configure the reporter
         coverageReporter: {
-            type: 'lcov',
-            dir: 'coverage/'
+            type: "lcov",
+            dir: "coverage/"
         },
 
 
